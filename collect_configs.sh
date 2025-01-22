3d6549aee7042f3c35b6a23b8e165dc5f3b9f0f1
nfoert <nofoert@gmail.com>
2025-01-22 11:32:11 -0500

    feat: Make shell scripts have colors

--- collect_configs.sh
+++ collect_configs.sh
@@ -1,6 +1,17 @@
-echo Press enter to collect all configuration files from the system
+GREEN='\e[32m'
+CYAN='\e[36m'
+GRAY='\e[90m'
+PURPLE='\e[35m'
+BOLD='\e[1m'
+NC='\e[0m'
+
+echo -e "\n    ${PURPLE}${BOLD}niri-dotfiles${NC} ${PURPLE}by nfoert${NC}"
+echo -e "    ${GRAY}- https://github.com/nfoert/niri-dotfiles${NC}\n"
+
+echo -e "${CYAN}Press ${BOLD}enter${NC}${CYAN} to collect all configuration files from the system into this repository${NC}"
 read
 
+echo -e "${GRAY}"
 mkdir ./niri
 mkdir ./niri/scripts
 mkdir ./waybar
@@ -16,5 +27,6 @@ cp /etc/xdg/fuzzel/fuzzel.ini ./fuzzel/fuzzel.ini
 cp ~/.config/nwg-bar/bar.json ./nwg-bar/bar.json
 cp ~/.config/nwg-bar/style.css ./nwg-bar/style.css
 cp ~/.config/mako/config ./mako/config
+echo -e "${NC}"
 
-echo Collected all configuration files from the system and copied them to the repo
+echo -e "\n${GREEN}${BOLD}Done!${NC}"
--- copy_configs.sh
+++ copy_configs.sh
@@ -1,6 +1,17 @@
-echo Press enter to copy all configuration files from this repo to their place on the system
+GREEN='\e[32m'
+YELLOW='\e[33m'
+GRAY='\e[90m'
+PURPLE='\e[35m'
+BOLD='\e[1m'
+NC='\e[0m'
+
+echo -e "\n    ${PURPLE}${BOLD}niri-dotfiles${NC} ${PURPLE}by nfoert${NC}"
+echo -e "    ${GRAY}- https://github.com/nfoert/niri-dotfiles${NC}\n"
+
+echo -e "${YELLOW}Press ${BOLD}enter${NC}${YELLOW} to copy all configuration files from this repository into their place on the system${NC}"
 read
 
+echo -e "${GRAY}"
 cp ./niri/config.kdl ~/.config/niri/config.kdl
 cp ./niri/scripts/* ~/.config/niri/scripts
 cp ./waybar/config ~/.config/waybar/config
@@ -9,5 +20,6 @@ cp ./fuzzel/fuzzel.ini /etc/xdg/fuzzel/fuzzel.ini
 cp ./nwg-bar/bar.json ~/.config/nwg-bar/bar.json
 cp ./nwg-bar/style.css ~/.config/nwg-bar/style.css
 cp ./mako/config ~/.config/mako/config
+echo -e "${NC}"
 
-echo Copied configuration files
+echo -e "\n${GREEN}${BOLD}Done!${NC}"
